1e236c004b62f5255f6c08be288adebec1a9b851
shreya0522 <139756143+shreya0522@users.noreply.github.com>
2024-02-01 22:32:18 +0530

    Update Checkout.groovy

--- src/org/mygurukulam/common/Checkout.groovy
+++ src/org/mygurukulam/common/Checkout.groovy
@@ -1,6 +1,6 @@
 package org.mygurukulam.common
 
-def call(String url , String branch) {
-            echo "Cloning repo ${url} from branch ${branch}";
-            git branch: "${branch}", url: "${url}"
+def call(String url , String branch String creds) {
+            echo "Cloning repo ${url} from branch ${branch} using creds ${creds}";
+            git branch: "${branch}", url: "${url}" credentialsId: "${creds}";
     }
